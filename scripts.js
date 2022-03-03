8fe229d3bf05786418cc42fc8e0cacb3698129af
MareikeHavemann <tipptipptapp@yahoo.de>
2022-03-03 20:07:23 +0100

    Added code for new items to li of items

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
 function newItem(){
 
+  // 1. Adding a new item to the list of items:
+
+  let li = $('<li></li>');
+  // inputValue defined by val function on #input class
+  let inputValue = $('#input').val();
+  // Adds inputValue to li
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert("Please write something");
+  } else {
+    $('#list').append(li);
+  }
+
 
 }
